fd5118cbdcd802e8838c2f9504b4d4785c6d45b4
Dipanshu-Jaiswal <djaiseal0807@gmail.com>
2025-09-22 23:48:49 +0530

    changes1

--- script.js
+++ script.js
@@ -10,19 +10,24 @@ const symbolCheck = document.querySelector("#symbols") ;
 const indicator = document.querySelector("[data-indicator]") ;
 const generateBtn = document.querySelector(".Generate-button") ;
 const allCheckBox = document.querySelectorAll("input[type=checkbox]") ;
+const symbol = '~!@#$%^&*()_+-={}|;:"<>?,./';
 
-let password = "" ;
-let passwordLength = 20;
-let checkCount = 1 ;
+
+//initially
+let password = "";
+let passwordLength = 10;
+let checkCount = 1;
 handleSlider();
-//set color of Strength Circle is GREY
+//ste strength circle color to grey
 
 
-function handleSlider()  {
-    inputSlider.value = passwordLength ;
-    lengthDisplay.innerText = passwordLength ;
-
+//set passwordLength
+function handleSlider() {
+    inputSlider.value = passwordLength;
+    lengthDisplay.innerText = passwordLength;
+    //or kuch bhi karna chahiye ? - HW
 }
+
 
 function setIndecator(color){
     indicator.style.backgroundcolor = color ;
@@ -31,7 +36,69 @@ function setIndecator(color){
 }
 
 function getRndInteger(min, max){
+    Math.floor(Math.random() * (max - min)) +  min  ;
+
 
 }
 
+function generateRandomNumber(){
+    return getRndInteger(0 , 9);
+}
 
+function generateLowerCase(){
+    return String.fromCharCode(getRndInteger(97 , 123)) ;
+}
+
+function generateUpperCase(){
+    return String.fromCharCode(getRndInteger(65 , 91));
+}
+
+function generateSymbol(){
+    const rndNum = getRndInteger(0 , symbol.length);
+    return symbol.charAt(rndNum) ;
+}
+
+function calStrength(){
+    let hasUpper = false ;
+    let hasLower = false ;
+    let hasNum = false ;
+    let hasSym = false ;
+
+    if(uppercaseCheck.checked) hasUpper = true ;
+    if(lowercaseCheck.checked) hasLower = true ;
+    if(numberCheck.checked) hasNum = true ;
+    if(symbolCheck.checked) hasSym = true ;
+
+    if(hasUpper && hasLower (hasNum || hasSym) && passwordLength >= 8){
+        setIndecator("#0f0");
+    }
+    else if((hasUpper || hasLower) && (hasNum || hasSym) && passwordLength >=6){
+        setIndecator("#0ff0") ;
+    }
+    else {
+        setIndecator("#0f00") ;
+    }
+}
+
+async function copyContent() {
+    try{
+        await navigator.clipboard.writeText(passDisplay);
+        copyMsg.innerText = "Copied";
+    }
+    catch(e){
+        copyMsg.innerText = "Failed" ;        
+    }
+
+    //to make copy wala span(copied text) visible 
+    copyMsg.classList.add("active");
+
+    setTimeout( () => {
+        copyMsg.classList.remove("active") ;
+    } , 2000)
+}
+
+
+inputSlider.addEventListener('input', (e) => {
+    passwordLength = e.target.value;
+    handleSlider();
+})
